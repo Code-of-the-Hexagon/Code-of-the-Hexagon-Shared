02c6dcd06795a5a332f70407df6b79cd8d836a8d
Martynas Janušaitis <41643365+MartynasJanusaitis@users.noreply.github.com>
2023-09-09 19:37:50 +0300

    Revert "Add shared world class"

--- SharedWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿using Code_of_the_Hexagon_Server.Shared.CoordinateSystem;
-
-namespace Code_of_the_Hexagon_Server.Shared
-{
-    public class SharedWorld
-    {
-        public required List<ObjectCoordinates> HexTileList { get; init; }
-    }
-}
