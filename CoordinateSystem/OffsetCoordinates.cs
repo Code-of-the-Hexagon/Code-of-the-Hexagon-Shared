1ba65ac4c3ffe8b134fe0f5fb6ae0dd1d1a6e98f
TomasMarcinkevicius <tomasmarcinkevicius25@gmail.com>
2023-09-09 20:13:35 +0300

    Add class

---
+++ CoordinateSystem/OffsetCoordinates.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.CoordinateSystem
+{
+    public class OffsetCoordinates
+    {
+        public required int X { get; init; }
+        public required int Y { get; init; }
+
+        public override string ToString () =>
+            $"(X = {X}, Y = {Y})";
+    }
+}
--- GameConstants.cs
+++ GameConstants.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public static class GameConstants
+namespace Shared
 {
-    public static class CameraConstants
+    public static class GameConstants
     {
-        public const float FreeDragSpeed = 0.03f;
-        public const float FreeRotateSpeed = 0.02f;
+        public static class CameraConstants
+        {
+            public const float FreeDragSpeed = 0.03f;
+            public const float FreeRotateSpeed = 0.02f;
+        }
     }
 }
