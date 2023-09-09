f5d033794c83015b604dac0cbf7d1d8dd35193be
Martynas Janušaitis <martynasjanusaitis1@gmail.com>
2023-09-09 12:05:08 +0300

    Added class for camera constants

---
+++ GameConstants.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameConstants
+{
+    public static class CameraConstants
+    {
+        public const float FreeDragSpeed = 0.02f;
+        public const float FreeRotateSpeed = 0.02f;
+    }
+}
