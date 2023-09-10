84be0706a02abc93a10a05c049fb42207920fd94
Martynas Janušaitis <martynasjanusaitis1@gmail.com>
2023-09-10 12:47:03 +0300

    Hotfix updated keyboard.cs to not use type KeyCode

--- KeyboardSettings.cs
+++ KeyboardSettings.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public static class KeyboardSettings
 {
-    public const KeyCode MoveCameraForwards = KeyCode.W;
-    public const KeyCode MoveCameraBackwards = KeyCode.S;
-    public const KeyCode MoveCameraLeft = KeyCode.A;
-    public const KeyCode MoveCameraRight = KeyCode.D;
-    public const KeyCode RotateCameraLeft = KeyCode.Q;
-    public const KeyCode RotateCameraRight = KeyCode.E;
-    public const KeyCode IncreaseCameraSpeed = KeyCode.LeftShift;
+    public const string MoveCameraForwards = "W";
+    public const string MoveCameraBackwards = "S";
+    public const string MoveCameraLeft = "A";
+    public const string MoveCameraRight = "D";
+    public const string RotateCameraLeft = "Q";
+    public const string RotateCameraRight = "E";
+    public const string IncreaseCameraSpeed = "LeftShift";
+
 }
